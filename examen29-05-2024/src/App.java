384c3453fa91157751c06e4acdaabe91c54d21cf
Oswaldo Chilin <oswaldochilin49@gmail.com>
2024-05-29 11:55:34 -0600

    ordenara sin mayor a menor

--- examen29-05-2024/src/App.java
+++ examen29-05-2024/src/App.java
@@ -1,51 +1,55 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class App extends Empleado {
     public static void main(String[] args) throws Exception {
 
-        ArrayList<Empleado> Empleados = new ArrayList<>();
+        ArrayList<Empleado> empleados = new ArrayList<>();
         Empleado emp = new Empleado();
 
         Scanner teclado = new Scanner(System.in);
 
         String decicString;
 
-         Empleados.add(new Empleado(1, "juan", "martinez", "la liberta", 1));
+        //   empleados.add(new Empleado(1, "juan", "martinez", "la liberta", 1));
 
-         Empleados.add(new Empleado(3, "chepe", "martinez", "la liberta", 1));
-         Empleados.add(new Empleado(2, "german", "martinez", "la liberta", 1));
+        //   empleados.add(new Empleado(3, "chepe", "martinez", "la liberta", 1));
+        //   empleados.add(new Empleado(2, "german", "martinez", "la liberta", 1));
 
-        // while (true) {
+         while (true) {
 
-        //     System.out.println("ingrese el Id: ");
-        //     emp.setId(teclado.nextInt());
+            System.out.println("ingrese el Id: ");
+            emp.setId(teclado.nextInt());
 
-        //     System.out.println("ingrese el nombre: ");
-        //     emp.setNombre(teclado.next());
+            System.out.println("ingrese el nombre: ");
+            emp.setNombre(teclado.next());
 
-        //     System.out.println("ingrese el apellido: ");
-        //     emp.setApellido(teclado.next());
+            System.out.println("ingrese el apellido: ");
+            emp.setApellido(teclado.next());
 
-        //     System.out.println("ingrese el departamento: ");
-        //     emp.setDepartamento(teclado.next());
+            System.out.println("ingrese el departamento: ");
+            emp.setDepartamento(teclado.next());
 
-        //     System.out.println("ingrese el salario: ");
-        //     emp.setSalario( Double.parseDouble( teclado.next()));
+            System.out.println("ingrese el salario: ");
+            emp.setSalario( Double.parseDouble( teclado.next()));
 
-        //     System.out.println("");
+            System.out.println("");
 
-        //     System.out.println("desea detener la carga de empledos (s) , (n): ");
-        //     decicString = teclado.next();
+            System.out.println("desea detener la carga de empledos (s) , (n): ");
+            decicString = teclado.next();
 
-        //     Empleados.add(emp);
+            empleados.add(emp);
 
-        //     if (decicString.equals("n")) break;
-        // }
+            if (decicString.equals("n")) break;
+        }
 
-            
-        Iterator<Empleado> iterator = Empleados.iterator();            
+       
+            Collections.sort(empleados, Comparator.comparingInt(Empleado :: getId));
+        Iterator<Empleado> iterator = empleados.iterator();            
         while (iterator.hasNext()) {
 
             Empleado empo = iterator.next();
--- examen29-05-2024/src/Empleado.java
+++ examen29-05-2024/src/Empleado.java
@@ -1,9 +1,5 @@
-import java.util.Collections;
-import java.util.Comparator;
-
 public class Empleado {
     
-
     private int id;
     private String nombre;
     private String apellido;
